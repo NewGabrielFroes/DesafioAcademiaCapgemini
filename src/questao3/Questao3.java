951943c04446e6c63a049ab83577a6c244d1e830
Gabriel Froes <79543726+NewGabrielFroes@users.noreply.github.com>
2022-02-21 11:45:21 -0300

    Add files via upload

--- src/questao3/Questao3.java
+++ src/questao3/Questao3.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 
 public class Questao3 {
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Palavra: ");
         String palavra = scanner.next();
